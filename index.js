cc6b66cd1b500ae762f72b284b6b2ea6277a3402
chenyuhao <cccchenyuhao@gmail.com>
2023-11-11 05:23:06 +0800

    chat

---
+++ index.js
@@ -0,0 +1,12 @@
+import { openai } from "./openai.js";
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+}
+
+main();
