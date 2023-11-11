59a427f09060cf53de61ce77b4f4862b876f23f2
chenyuhao <cccchenyuhao@gmail.com>
2023-11-11 16:06:57 +0800

    chat

--- chat.js
+++ chat.js
@@ -1,17 +1,51 @@
+import "dotenv/config";
+import readline from "node:readline";
 import { openai } from "./openai.js";
-import * as readline from "node:readline/promises";
-import { stdin as input, stdout as output } from "node:process";
 
-// readline
-const rl = readline.createInterface({ input, output });
-// const answer = await rl.question("What do you think of Node.js? ");
-// console.log(`Thank you for your valuable feedback: ${answer}`);
-// rl.close();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const newMessage = async (history, message) => {
-  const completion = await openai.chat.completions.create({
+  const chatCompletion = await openai.chat.completions.create({
     messages: [...history, message],
     model: "gpt-3.5-turbo",
   });
-  return completion.choices[0];
+
+  return chatCompletion.choices[0].message;
 };
+
+const formatMessage = (userInput) => ({ role: "user", content: userInput });
+
+const chat = () => {
+  const history = [
+    {
+      role: "system",
+      content: `You are a helpful AI assistant. Answer the user's questions to the best of you ability.`,
+    },
+  ];
+  const start = () => {
+    rl.question("You: ", async (userInput) => {
+      if (userInput.toLowerCase() === "exit") {
+        rl.close();
+        return;
+      }
+
+      const userMessage = formatMessage(userInput);
+      const response = await newMessage(history, userMessage);
+
+      history.push(userMessage, response);
+      console.log(`\n\nAI: ${response.content}\n\n`);
+      start();
+    });
+  };
+
+  start();
+};
+
+console.log("Chatbot initialized. Type 'exit' to end the chat.");
+
+console.log("\n\nAI: How can I help you today?\n\n");
+
+chat();
